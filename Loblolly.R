781a7b4db462710aba0fdd47f416d181b11c5890
Oliver Bott <o.bott@mpp.hertie-school.org>
2014-09-26 15:58:35 +0200

    Loblolly.R
    Secondary file showing Loblolly growth over time

---
+++ Loblolly.R
@@ -0,0 +1,19 @@
+# Explore the Loblolly data set and its variables in R
+
+?Loblolly
+
+data(Loblolly)
+
+attach(Loblolly)
+
+names(Loblolly)
+
+
+# Descriptive statistics for Loblolly
+
+summary(Loblolly)
+
+plot(Loblolly$age, Loblolly$height, 
+  main = 'Height of Loblolly trees over time', 
+  xlab = 'Age in years', 
+  ylab = 'Height in ft.')
